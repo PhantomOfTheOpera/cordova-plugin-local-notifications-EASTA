cb8bac71eee50d0292544ea7b392352fc7188697
PhantomOfTheOpera <vyacheslav.potseluyko@gmail.com>
2016-12-23 19:09:35 +0300

    if notification is going data from button isn\'t handled. Trying to explore and fix

--- src/android/ClickActivity.java
+++ src/android/ClickActivity.java
@@ -46,9 +46,9 @@ public class ClickActivity extends de.appplant.cordova.plugin.notification.Click
 
         launchApp();
 
-        if (notification.getOptions().isOngoing())
+        /*if (notification.getOptions().isOngoing())
             return;
-
+        */
         String event = notification.isRepeating() ? "clear" : "cancel";
         LocalNotification.fireEvent(event, notification);
     }
